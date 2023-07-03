7b2118a2fa859ded972f579ae4877db768664be3
roy-Pritom <pritombd.hitcse2020@gmail.com>
2023-07-03 19:57:35 +0600

    add my code

--- js/script.js
+++ js/script.js
@@ -10,20 +10,6 @@ menuIcon.onclick=()=>{
 
 window.onscroll=()=>{
 
-// sections.forEach(sec=>{
-//     let top=window.scrollY;
-//     let offset=sec.offsetTop-150;
-//     let height=sec.offsetHeight;
-//     let id=sec.setAttribute('id')
-//     if(top>=offset && top < offset+height)
-//     {
-//         navLinks.forEach(links=>{
-//             links.classList.remove('active');
-//             document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
-//         })
-//     }
-// })
-
 let header=document.querySelector('header');
 header.classList.toggle('sticky',window.scrollY>100)
 menuIcon.classList.remove('bx-x');
